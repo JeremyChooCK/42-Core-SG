b9354d700a828590e5af43546ee8c75f19e1bfd0
Jeremy Choo <113224356+JeremyChooCK@users.noreply.github.com>
2023-09-10 22:47:15 +0800

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -10,18 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+​
+
 #include "libft.h"
 
+​
+
 void	*ft_memcpy(void *dest, const void *src, size_t n)
+
 {
-	unsigned char		*d;
+
 	const unsigned char	*s;
 
-	if (!dest && !src)
-		return (0);
-	d = dest;
-	s = src;
+	unsigned char		*d;
+
+​
+
+	if (dest == src || !n)
+
+		return (dest);
+
+	s = (const unsigned char *) src;
+
+	d = (unsigned char *) dest;
+
 	while (n--)
+
 		*d++ = *s++;
+
 	return (dest);
+
 }
