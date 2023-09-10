0878dc58680ce74bed539e4368ea36cc87ea27c5
Jeremy Choo <113224356+JeremyChooCK@users.noreply.github.com>
2023-09-10 22:23:23 +0800

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -9,80 +9,27 @@
 /*   Updated: 2023/09/06 12:15:08 by jechoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include<stddef.h>
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	unsigned char		*d;
-	const unsigned char	*s;
 
-	if (!dest && !src)
-		return (0);
-	d = dest;
-	s = src;
-	while (n--)
-		*d++ = *s++;
-	return (dest);
-}
-
-static void	*ft_reverse_memcpy(void *dest, const void *src, size_t n)
-
-{
-
-	const unsigned char	*s;
-
-	unsigned char		*d;
-
-
-
-	s = (const unsigned char *) src;
-
-	d = (unsigned char *) dest;
-
-	while (n--)
-
-		*(d + n) = *(s + n);
-
-	return (dest);
-
-}
+#include "libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
-
 {
+	unsigned char		*d;
+	const unsigned char	*s;
 
-	if (dest == (void *)src || n == 0)
-
-		return (dest);
-
-	if (dest > src && dest - src < (long int) n)
-
-		return (ft_reverse_memcpy(dest, src, n));
-
-	return (ft_memcpy(dest, src, n));
-
-}
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-    char src[50] = "Hello, world!";
-    char dest[50];
-
-    // Copy 'src' to 'dest' using your ft_memmove
-    ft_memmove(dest, src, strlen(src) + 1);
-
-    // Now try to move the first 7 characters in 'dest' forward by 5 positions
-    ft_memmove(dest + 5, dest, 7);
-
-    printf("Result using ft_memmove: %s\n", dest);
-
-    // Reset 'dest'
-    strcpy(dest, src);
-
-    // Now try the same thing using the standard memmove
-    memmove(dest + 5, dest, 7);
-
-    printf("Result using memmove: %s\n", dest);
-
-    return 0;
+	d = dest;
+	s = src;
+	if (d < s)
+	{
+		while (n--)
+			*d++ = *s++;
+	}
+	else if (d > s)
+	{
+		d += n;
+		s += n;
+		while (n--)
+			*(--d) = *(--s);
+	}
+	return (dest);
 }
