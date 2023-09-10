ae11cdf2dd5d3b924291a72f36a3c470c235cb58
Jeremy Choo <113224356+JeremyChooCK@users.noreply.github.com>
2023-09-10 22:26:07 +0800

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -12,24 +12,42 @@
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+static void	*ft_reverse_memcpy(void *dest, const void *src, size_t n)
+
 {
-	unsigned char		*d;
+
 	const unsigned char	*s;
 
-	d = dest;
-	s = src;
-	if (d < s)
-	{
-		while (n--)
-			*d++ = *s++;
-	}
-	else if (d > s)
-	{
-		d += n;
-		s += n;
-		while (n--)
-			*(--d) = *(--s);
-	}
+	unsigned char		*d;
+
+​
+
+	s = (const unsigned char *) src;
+
+	d = (unsigned char *) dest;
+
+	while (n--)
+
+		*(d + n) = *(s + n);
+
 	return (dest);
+
+}
+
+​
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+
+{
+
+	if (dest == (void *)src || n == 0)
+
+		return (dest);
+
+	if (dest > src && dest - src < (long int) n)
+
+		return (ft_reverse_memcpy(dest, src, n));
+
+	return (ft_memcpy(dest, src, n));
+
 }
