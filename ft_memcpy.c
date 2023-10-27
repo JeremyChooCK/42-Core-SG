e86b2cd5c66b406ff8f3b54c04ff328e4adcea65
jeremy <jeremychoo85@gmail.com>
2023-10-27 12:22:21 +0800

    fixed with original version

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -10,34 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-​
-
 #include "libft.h"
 
-​
-
 void	*ft_memcpy(void *dest, const void *src, size_t n)
-
 {
-
+	unsigned char		*d;
 	const unsigned char	*s;
 
-	unsigned char		*d;
-
-​
-
-	if (dest == src || !n)
-
-		return (dest);
-
-	s = (const unsigned char *) src;
-
-	d = (unsigned char *) dest;
-
+	if (!dest && !src)
+		return (0);
+	d = dest;
+	s = src;
 	while (n--)
-
 		*d++ = *s++;
-
 	return (dest);
-
 }
--- ft_memmove.c
+++ ft_memmove.c
@@ -12,42 +12,24 @@
 
 #include "libft.h"
 
-static void	*ft_reverse_memcpy(void *dest, const void *src, size_t n)
-
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-
+	unsigned char		*d;
 	const unsigned char	*s;
 
-	unsigned char		*d;
-
-​
-
-	s = (const unsigned char *) src;
-
-	d = (unsigned char *) dest;
-
-	while (n--)
-
-		*(d + n) = *(s + n);
-
+	d = dest;
+	s = src;
+	if (d < s)
+	{
+		while (n--)
+			*d++ = *s++;
+	}
+	else if (d > s)
+	{
+		d += n;
+		s += n;
+		while (n--)
+			*(--d) = *(--s);
+	}
 	return (dest);
-
-}
-
-​
-
-void	*ft_memmove(void *dest, const void *src, size_t n)
-
-{
-
-	if (dest == (void *)src || n == 0)
-
-		return (dest);
-
-	if (dest > src && dest - src < (long int) n)
-
-		return (ft_reverse_memcpy(dest, src, n));
-
-	return (ft_memcpy(dest, src, n));
-
 }
